e9277f37c1bcbe8ceabd91a0e2fa11f94b779639
HecateET <1060526453@qq.com>
2022-02-13 11:39:56 +0800

    初版目录搭建

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    title: '阿源 Hecate的博客',
+    description: '阿源 Hecate的博客 记录前端学习、日常生活、阅读、美食、运动等感想日记',
+    themeConfig: {
+        logo:'../assets/img/logo.png',
+        nav: [
+            { text: '首页', link: '/' },
+            { 
+                text: '阿源 Hecate的博客', 
+                items: [
+                    { text: 'Github', link: 'https://github.com/HecateET' },
+                    { text: '掘金', link: 'https://juejin.cn/user/465848660675048/posts' },
+                    { text: '美食日记｜小红书',link:'https://www.xiaohongshu.com/user/profile/5e94f5de000000000100a3b1?xhsshare=CopyLink&appuid=5e94f5de000000000100a3b1&apptime=1644722473'}
+                ]
+            }
+        ],
+        sidebar:[
+            {
+
+            }
+        ]
+    }
+  }
